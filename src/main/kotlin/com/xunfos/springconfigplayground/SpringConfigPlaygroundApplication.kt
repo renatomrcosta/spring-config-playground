329e6e6dfefaec7b87d90357e10a19d6a9449c49
Renato Costa <renato.rezende.costa@gmail.com>
2021-12-10 14:19:27 +0100

    initial dummy service draft

---
+++ src/main/kotlin/com/xunfos/springconfigplayground/SpringConfigPlaygroundApplication.kt
@@ -0,0 +1,63 @@
+package com.xunfos.springconfigplayground
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
+
+@SpringBootApplication
+class SpringConfigPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringConfigPlaygroundApplication>(*args)
+}
+
+@RestController
+class MyController(
+    private val client: DelayClient,
+) {
+    @GetMapping("/delay")
+    suspend fun delay(): String = client.getValueDelayed()
+}
+
+
+@Configuration
+class MyHttpConfiguration(
+    @Value("\${http.defaultTimeout}")
+    private val defaultTimeout: Long,
+    private val webClientBuilder: WebClient.Builder,
+) {
+    @Bean
+    fun webClient(): WebClient {
+        val httpClient = HttpClient.create().responseTimeout(Duration.ofMillis(defaultTimeout))
+        return webClientBuilder.clientConnector(ReactorClientHttpConnector(httpClient)).build()
+    }
+}
+
+@Component
+class DelayClient(
+    private val webClient: WebClient,
+    @Value("\${client.requestDelaySeconds}")
+    private val requestDelaySeconds: Int,
+    @Value("\${client.url}")
+    private val url: String
+) {
+    suspend fun getValueDelayed(): String = webClient
+        .get()
+        .uri("$url$delayUri/$requestDelaySeconds")
+        .retrieve()
+        .awaitBody()
+
+    companion object {
+        private const val delayUri = "/delay"
+    }
+}
